c05d284b3da96319aefa0bb34649bf2c81f9b44b
Mayerly <mayerlypirela@gmail.com>
2025-01-30 12:51:09 -0300

    update app e index

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 1; 
 
+console.log(numeroSecreto);
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
-function intentoDeUsuario(){
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+        //El usuario no acertó
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor');  
+        } else{
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;    
 }
 
 asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p', 'Indica un número del 1 al 100');
+asignarTextoElemento('p', 'Indica un número del 1 al 10');
